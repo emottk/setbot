9ec56aa1c3408a8fb01a19b08ed57d891e14a28a
emottk <emily.kowal@postlight.com>
2019-07-20 20:39:35 -0400

    first commit with set score and past score working

---
+++ app/models.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from app import db
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	slack_username = db.Column(db.String(64), index=True, unique=True)
+	slack_userid = db.Column(db.Integer, index=True, unique=True)
+	set_scores = db.relationship('Score', backref='user', lazy='dynamic')
+
+	def __repr__(self):
+		return '<User {}>'.format(self.slack_username)
+	
+
+class Score(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	orig_input = db.Column(db.String(64))
+	value = db.Column(db.Time, index=True)
+	timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+	def __repr__(self):
+		return '<Score {}>'.format(self.value)
+
+
---
+++ app/routes.py
@@ -0,0 +1,69 @@
+import os
+
+from app import app, db
+from app.models import User, Score
+from flask import abort, Flask, jsonify, request
+from datetime import datetime
+import pytz
+
+
+def is_request_valid(request):
+	is_token_valid = request.form['token'] == os.environ['SLACK_VERIFICATION_TOKEN']
+	is_team_id_valid = request.form['team_id'] == os.environ['SLACK_TEAM_ID']
+
+	return is_token_valid and is_team_id_valid
+
+
+@app.route('/setscore', methods=['POST'])
+def set_score():
+	if not is_request_valid(request):
+		abort(400)
+	
+	user_id = request.form["user_id"]
+	user_name = request.form["user_name"]
+	orig_input = request.form["text"]
+	
+	if not User.query.filter_by(slack_userid=user_id).first():
+		user = User(slack_userid=user_id, slack_username=user_name)
+		db.session.add(user)
+		db.session.commit()
+
+	else:
+		user = User.query.filter_by(slack_userid=user_id).first()
+	
+	try:
+		value = datetime.strptime(orig_input, "`%H hours %M minutes and %S.%f seconds`").time()
+	except ValueError:
+		return jsonify(
+			response_type='in_channel',
+			text="please input your score in a code block using backticks, in set score form ex: `0 hours 00 minutes and 0.00 seconds`")
+ 
+	score = Score(orig_input=orig_input, user=user, value=value)
+	db.session.add(score)
+	db.session.commit()
+
+	return jsonify(
+		response_type='in_channel',
+		text=f'Your time {request.form["text"]} has been saved! Good job today!',
+	)
+
+@app.route('/past_scores', methods=['POST'])
+def past_scores():
+	if not is_request_valid(request):
+        	abort(400)
+
+	user_id = request.form["user_id"]
+	return_text = ''
+	user = User.query.filter_by(slack_userid=user_id).first()
+	scores = user.set_scores.all()
+	timezone = pytz.timezone('America/New_York')
+
+	for val in scores:
+		return_text += f'{val.value.strftime("`%H hours %M minutes and %S.%f seconds`")} on *{val.timestamp.strftime("%c")}*\n'
+	
+	print(f'{val.value.strftime("`%H hours %M minutes and %S.%f seconds`")} on *{val.timestamp.strftime("%c")}*\n')
+
+	return jsonify(
+		response_type='in_channel',
+		text=f'Here are your past scores!\n{return_text}',
+	)
